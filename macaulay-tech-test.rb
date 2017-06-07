ffd38012f09249c4fb2d9f73c1d945b45e4d8cbd
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-06-06 20:29:40 -0600

    add best rated video

--- macaulay-tech-test.rb
+++ macaulay-tech-test.rb
@@ -1,9 +1,7 @@
 require 'json'
-require 'pry'
 # Iterate through data and:
 ## Return the ~ title ~ of the most likes v. dislikes
 ## Average % of likes vs. dislikes for 12 videos
-## Total number of views
 ## ~ Bonus ~ the average time interval between videos
 
 file        = File.read('zoella_videos.json')
@@ -11,12 +9,22 @@ videos_hash = JSON.parse(file)
 videos      = videos_hash['videos']
 
 def calculation_loop(videos)
-  total_views = 0
+  total_views     = 0
+  rating_average  = 0
+  most_liked      = ""
+  highest_average = 0
   videos.each do |video|
+    video_average = video['likes'] / video['dislikes']
+    if highest_average < video_average
+      highest_average = video_average
+      most_liked = video['title']
+    end
     total_views = total_views + video['views']
   end
   p "Total Views: #{total_views}"
+  p "#{most_liked}, highest % of likes vs. dislikes, at #{highest_average}%"
 end
+
 
 calculation_loop(videos)
 
@@ -25,3 +33,4 @@ calculation_loop(videos)
 
 # Done:
 # Read the JSON file, store data
+## Total number of views
