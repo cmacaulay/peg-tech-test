411aa5e54faadf9b6cd081d1f152e98871465c45
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-06-06 20:36:49 -0600

    add average % of likes v dislikes for all 12 videos

--- macaulay-tech-test.rb
+++ macaulay-tech-test.rb
@@ -1,6 +1,5 @@
 require 'json'
 # Iterate through data and:
-## Return the ~ title ~ of the most likes v. dislikes
 ## Average % of likes vs. dislikes for 12 videos
 ## ~ Bonus ~ the average time interval between videos
 
@@ -10,27 +9,35 @@ videos      = videos_hash['videos']
 
 def calculation_loop(videos)
   total_views     = 0
-  rating_average  = 0
   most_liked      = ""
-  highest_average = 0
+  averages        = []
+
   videos.each do |video|
+    highest_average = 0
     video_average = video['likes'] / video['dislikes']
+
     if highest_average < video_average
       highest_average = video_average
       most_liked = video['title']
     end
+
+    averages << video_average
     total_views = total_views + video['views']
   end
+  like_average = calculate_average(averages)
   p "Total Views: #{total_views}"
-  p "#{most_liked}, highest % of likes vs. dislikes, at #{highest_average}%"
+  p "Best Like Ratio: #{most_liked}"
+  p "Average of likes v. dislikes for all videos: #{like_average}%"
 end
 
+def calculate_average(data)
+  data.reduce(:+) / 12
+end
 
 calculation_loop(videos)
-
-
 
 
 # Done:
 # Read the JSON file, store data
 ## Total number of views
+## Return the ~ title ~ of the most likes v. dislikes
