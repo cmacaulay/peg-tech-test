e8449c10e00acef0e52791b9376aaeb687e999e1
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-06-06 20:01:44 -0600

    add json file read, parse json

--- macaulay-tech-test.rb
+++ macaulay-tech-test.rb
@@ -1,7 +1,14 @@
 require 'json'
-# Read the JSON file, store data in hashes
 # Iterate through data and:
 ## Return the ~ title ~ of the most likes v. dislikes
 ## Average % of likes vs. dislikes for 12 videos
 ## Total number of views
 ## ~ Bonus ~ the average time interval between videos
+
+file        = File.read('zoella_videos.json')
+videos_hash = JSON.parse(file)
+p videos_hash
+
+
+# Done:
+# Read the JSON file, store data
