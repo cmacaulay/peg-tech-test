0c0ae22a66fe3918662301c1f18d5d4e8704c1b5
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-06-06 19:51:40 -0600

    initial commit, breakdown of steps pseudocoded

---
+++ macaulay-tech-test.rb
@@ -0,0 +1,7 @@
+require 'json'
+# Read the JSON file, store data in hashes
+# Iterate through data and:
+## Return the ~ title ~ of the most likes v. dislikes
+## Average % of likes vs. dislikes for 12 videos
+## Total number of views
+## ~ Bonus ~ the average time interval between videos
